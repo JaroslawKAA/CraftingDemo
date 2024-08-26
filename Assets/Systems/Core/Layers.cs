4b531f1871de8838ca8fd52b4e82773cc796529e
JaroslawKAA <kaszczak.jaroslaw@outlook.com>
2024-08-26 22:31:51 +0200

    Create required items

---
+++ Assets/Systems/Core/Layers.cs
@@ -0,0 +1,6 @@
+using UnityEngine;
+
+public static class Layers
+{
+    public static LayerMask Item => LayerMask.NameToLayer("Item");
+}
--- Assets/Systems/Items/ItemInstance.cs
+++ Assets/Systems/Items/ItemInstance.cs
@@ -1,10 +1,39 @@
 using Sirenix.OdinInspector;
 using UnityEngine;
 
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
 namespace Systems.Items
 {
     public class ItemInstance : MonoBehaviour
     {
-        [SerializeField] [Required] Item item;
+        [SerializeField] [Required] [InlineEditor] Item item;
+
+#if UNITY_EDITOR
+        [Button]
+        void ConfigureItem()
+        {
+            if (!TryGetComponent(out Collider _))
+            {
+                gameObject.AddComponent<BoxCollider>();
+            }
+
+            if (!TryGetComponent(out Rigidbody _))
+            {
+                Rigidbody rb = gameObject.AddComponent<Rigidbody>();
+                rb.constraints = RigidbodyConstraints.FreezeRotationY
+                                 | RigidbodyConstraints.FreezeRotationX
+                                 | RigidbodyConstraints.FreezePositionZ
+                                 | RigidbodyConstraints.FreezePositionX
+                                 | RigidbodyConstraints.FreezeRotationZ;
+            }
+
+            gameObject.layer = Layers.Item;
+            
+            EditorUtility.SetDirty(gameObject);
+        }
+#endif
     }
 }
