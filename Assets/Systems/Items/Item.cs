ab29cbc0745f49dfd7ad15c0ec0eefc96a804291
JaroslawKAA <kaszczak.jaroslaw@outlook.com>
2024-08-26 21:42:19 +0200

    Create items data

---
+++ Assets/Systems/Items/Item.cs
@@ -0,0 +1,31 @@
+using Sirenix.OdinInspector;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Systems.Items
+{
+    [CreateAssetMenu(menuName = "Items/Item", fileName = "Item", order = 0)]
+    public class Item : ScriptableObject
+    {
+        [Title("Config")]
+        [SerializeField] [ReadOnly] [Required] string guid;
+        [SerializeField] [Required] string itemName;
+        [SerializeField] [Required] Sprite icon;
+        [SerializeField] [Required] GameObject prefab;
+        
+        public string Guid => guid;
+        public Sprite Icon => icon;
+        public GameObject Prefab => prefab;
+        public string ItemName => itemName;
+
+#if UNITY_EDITOR
+        [Button]
+        void GenerateGuid()
+        {
+            guid = System.Guid.NewGuid().ToString();
+            EditorUtility.SetDirty(this);
+        }
+#endif
+    }
+}
---
+++ Assets/Systems/Items/ItemInstance.cs
@@ -0,0 +1,10 @@
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Systems.Items
+{
+    public class ItemInstance : MonoBehaviour
+    {
+        [SerializeField] [Required] Item item;
+    }
+}
